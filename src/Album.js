e84c744c51c8f88dc1f7c2febd95066de13dd2c3
Amen Christ <amenphronesis@gmail.com>
2022-10-21 01:38:29 +0100

    Began arranging fetched data

--- src/Album.js
+++ src/Album.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -12,11 +12,47 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 
-const cards = [1, 2, 3];
-
 const theme = createTheme();
 
 export default function Album() {
+
+  function capitalize(word){
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+
+  const [pokedex, setPokedex ] = useState(() => {
+    const localData = localStorage.getItem('pokedex');
+    return localData ? JSON.parse(localData) : []
+  })
+
+  useEffect(() => {
+    localStorage.setItem('pokedex', JSON.stringify(pokedex))
+  }, [pokedex])
+
+  useEffect(() => {
+    let pokemons = [...pokedex];
+    if(pokedex.length < 151){
+      for(let i=pokedex.length+1; i<=151; i++){
+        fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then(result => result.json()).then(pokemon => {
+          const { id, name, height, weight, types, abilities } = pokemon
+          const pok = {
+            id,
+            name: capitalize(name),
+            imageUrl : pokemon.sprites.other['official-artwork']['front_default'],
+            height,
+            weight,
+            types: types.map(index => capitalize(index.type.name)).join(", "),
+            abilities: abilities.map(index => capitalize(index.ability.name)).join(", ")
+          }
+          pokemons.push(pok)
+          
+          setPokedex(pokemons)
+        })
+      }    
+    }
+    
+  },[pokedex])
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -29,7 +65,7 @@ export default function Album() {
             pb: 6,
           }}
         >
-          <Container maxWidth="md">
+          <Container maxWidth="md" align="center">
             <Typography
               component="h1"
               variant="h2"
@@ -50,39 +86,35 @@ export default function Album() {
                 noValidate
                 autoComplete="off"
                 >
-                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                <TextField id="outlined-basic" label="Search" variant="outlined" />
             </Box>
           </Container>
         </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
+        <Container sx={{ py: 1 }} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
+            {pokedex.map((pokemon, i) => (
+              <Grid item key={i} xs={12} sm={6} md={4}>
                 <Card
                   sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
                 >
                   <CardMedia
                     component="img"
-                    sx={{
-                      // 16:9
-                      pt: '56.25%',
-                    }}
-                    image="https://source.unsplash.com/random"
-                    alt="random"
+                    image={pokemon.imageUrl}
+                    alt={pokemon.name}
                   />
                   <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h5" component="h2">
-                      Heading
+                      {pokemon.name}
                     </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography>
+                    <Typography>Number: {pokemon.id}</Typography>
+                    <Typography>Types: {pokemon.types}</Typography>
+                    <Typography>Weight: {pokemon.weight/10}kg</Typography>
+                    <Typography>Height: {pokemon.height*10}cm</Typography>
+                    <Typography>Abilities: {pokemon.abilities}</Typography>
                   </CardContent>
                   <CardActions>
                     <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
                   </CardActions>
                 </Card>
               </Grid>
